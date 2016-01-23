6bca63e1cd7e7eae34d0a0bcf9942160f5a819da
alexandermendes <alexanderhmendes@gmail.com>
2016-01-23 20:03:44 +0000

    Update variables returned to view

--- libcrowds_statistics/view.py
+++ libcrowds_statistics/view.py
@@ -6,47 +6,45 @@ from flask import Blueprint
 from flask import render_template
 from pybossa.cache import site_stats
 from pybossa.cache import users as cached_users
+from pybossa.cache import projects as cached_projects
 from . import cache as extra_stats
 
 
 def index():
     """Return Global Statistics for the site."""
     title = "Statistics"
-    description = """Statistical analysis of crowdsourcing contributions
-                  and pretty charts."""
+    description = """Statistical analysis of contributions made via the
+                  LibCrowds crowdsourcing platform."""
 
     n_auth = site_stats.n_auth_users()
     n_anon = site_stats.n_anon_users()
     n_tasks = site_stats.n_tasks_site()
     n_task_runs = site_stats.n_task_runs_site()
-    top5_projects_24_hours = site_stats.get_top5_projects_24_hours()
-    top5_users_24_hours = site_stats.get_top5_users_24_hours()
-    locs = site_stats.get_locs()
-
-    top_users = cached_users.get_leaderboard(10)
-
     n_continents = extra_stats.n_continents()
     n_cities = extra_stats.n_cities()
     n_countries = extra_stats.n_countries()
-    top_countries = extra_stats.get_top_countries()
-
     n_auth_task_runs = extra_stats.n_auth_task_runs_site()
     n_tasks_completed = extra_stats.n_tasks_completed()
+    n_avg_days_active = extra_stats.n_avg_days_active()
+    n_published_projects = cached_projects.n_published()
+
+    top5_projects_24_hours = site_stats.get_top5_projects_24_hours()
+    top5_users_24_hours = site_stats.get_top5_users_24_hours()
     top5_projects_1_week = extra_stats.get_top5_projects_1_week()
     top5_users_1_week = extra_stats.get_top5_users_1_week()
-    n_avg_days_active = extra_stats.n_avg_days_active()
+
+    top_users = cached_users.get_leaderboard(10)
     top_10_percent = extra_stats.get_top_n_percent(10)
-    contributions_per_user = extra_stats.get_contributions_per_user()
-    active_days_per_user = extra_stats.get_active_days_per_user()
     task_runs_daily = extra_stats.get_task_runs_daily()
     users_daily = extra_stats.get_users_daily()
     dow = extra_stats.get_dow()
     hourly_activity = extra_stats.site_hourly_activity()
 
+    locs = site_stats.get_locs()
     show_locs = True if len(locs) > 0 else False
+    top_countries = extra_stats.get_top_countries()
 
-    stats = dict(n_active=n_active,
-                 n_auth=n_auth,
+    stats = dict(n_auth=n_auth,
                  n_anon=n_anon,
                  n_tasks=n_tasks,
                  n_task_runs=n_task_runs,
@@ -54,28 +52,22 @@ def index():
                  n_tasks_completed=n_tasks_completed,
                  n_countries=n_countries,
                  n_cities=n_cities,
-                 n_continents=n_continents)
+                 n_continents=n_continents,
+                 n_avg_days_active=n_avg_days_active,
+                 n_published_projects=n_published_projects)
 
     users = dict(label="User Statistics",
                  values=[dict(label='Anonymous', value=[0, n_anon]),
                          dict(label='Authenticated', value=[0, n_auth])])
 
-    projects = dict(label="Projects Statistics",
-                    values=[dict(label='Published',
-                                 value=[0, n_published_projects]),
-                            dict(label='Draft', value=[0, n_draft_projects])])
-
     tasks = dict(label="Task and Task Run Statistics",
                  values=[dict(label='Tasks', value=[0, n_tasks]),
                          dict(label='Task Runs', value=[1, n_task_runs])])
 
-    contributions_per_user = json.dumps(contributions_per_user)
-    active_days_per_user = json.dumps(active_days_per_user)
-
     return render_template('/stats.html', title=title,
                            description=description,
+                           stats=stats,
                            users=json.dumps(users),
-                           projects=json.dumps(projects),
                            tasks=json.dumps(tasks),
                            locs=json.dumps(locs),
                            show_locs=show_locs,
@@ -85,11 +77,8 @@ def index():
                            top5_projects_24_hours=top5_projects_24_hours,
                            top5_projects_1_week=top5_projects_1_week,
                            top_10_percent=top_10_percent,
-                           stats=stats,
                            task_runs_daily=json.dumps(task_runs_daily),
                            users_daily=json.dumps(users_daily),
                            dow=json.dumps(dow),
                            top_users=top_users,
-                           hourly_activity=json.dumps(hourly_activity),
-                           contributions_per_user=contributions_per_user,
-                           active_days_per_user=active_days_per_user)
+                           hourly_activity=json.dumps(hourly_activity))

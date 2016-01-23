9424027c2db06a5e6b78e41c5f8a3b72de0dbeeb
alexandermendes <alexanderhmendes@gmail.com>
2016-01-23 15:18:53 +0000

    Add test framework

---
+++ tests/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf8 -*-
+
+import sys
+import os
+import libcrowds_statistics as plugin
+
+# Use the PyBossa test suite
+sys.path.append(os.path.abspath("./pybossa/test"))
+
+
+def setUpPackage():
+    """Setup the plugin."""
+    from default import flask_app
+    with flask_app.app_context():
+        plugin_dir = os.path.dirname(plugin.__file__)
+        plugin.LibCrowdsStatistics(plugin_dir).setup()
